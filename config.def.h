39d6cfc5a77dcd29035d4e29950020d69e3e600c
Ilyes Khemakhem <ilyes.khemakhem@gmail.com>
2021-02-28 12:17:48 +0000

    switch to mutt and FontAwesome

--- config.def.h
+++ config.def.h
@@ -14,9 +14,9 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = {"Mononoki Nerd Font:size=10:antialias=true:autohint=true",
-                                        "JoyPixels:size=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {"monospace:size=11", "FontAwesome 5 Free Solid:size=11"};
+                                        /* "JoyPixels:size=11:antialias=true:autohint=true"}; */
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -121,7 +121,8 @@ static const char *rofirun[]  = { "rofi", "-show", "run", "-show-icons", "-confi
 static const char *rofidrun[] = { "rofi", "-show", "drun", "-show-icons", "-config", "~/.config/rofi/themes/center.rasi", NULL };
 static const char *rofiwin[]  = { "rofi", "-show", "window", "-show-icons", "-config", "~/.config/rofi/themes/center.rasi", NULL };
 static const char *firefox[]  = { "firefox", NULL };
-static const char *mail[]     = { "thunderbird", NULL };
+/* static const char *mail[]     = { "thunderbird", NULL }; */
+static const char *mail[]     = { TERMINAL, "-e", "neomutt", NULL };
 static const char *fileman[]  = { "thunar", NULL };
 static const char *emacs[]    = { "emacs", NULL };
 
